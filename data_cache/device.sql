c067305ab5145fcf69caf9366d5df179ef9f4fbd
neon185a-axe <neon185a.axe@yandex.ru>
2025-05-11 12:04:53 +0300

    Обновил стартовые данные для базы данных

--- data_cache/device.sql
+++ data_cache/device.sql
@@ -6,4 +6,9 @@ INSERT INTO device (device_name,company_id,type_device_id) VALUES
 	 ('7x40',2,1),
 	 ('Arolla',1,2),
 	 ('Scenius unico',1,4),
-   ('K90', 1, 5);
+	 ('Strobe 1000',2,5),
+	 ('37x40',2,3),
+	 ('K10',1,1);
+INSERT INTO device (device_name,company_id,type_device_id) VALUES
+	 ('F230',2,4),
+	 ('Spot400',2,2);
